8bc51f9af1f3d066ea0183f1b3736394db14f468
dennisemmerson <dennismichaelemmerson@gmail.com>
2021-09-18 11:26:22 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// This app uses the API from https://api.quotable.io/random to show random quotations
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: {},
+    }
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.newQuote();
+  }
+
+  // fetch a random quotation using the quotable API and assign it to the state variable "quote"
+  newQuote() {
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      let newQuote = {quoteText: data.content, quoteAuthor: data.author}
+      this.setState((state) => (
+        {quote:  newQuote}
+      ));
+      console.log(`${data.content} —${data.author}`)
+    });
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+          <div  className="inner">
+          <div>
+            <h2 id="text"><span id="block-quote">"</span>   {this.state.quote.quoteText}<span id="block-quote">   "</span></h2>
+            <p id="author">{this.state.quote.quoteAuthor}</p>
+            <button className="button-53" id="new-quote" onClick={this.newQuote}>New Quote</button>
+            <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">Twitter</a>
+            </div>
+          </div>
+      </div>
+    );
+  }
+};
 
 export default App;
